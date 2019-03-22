2c402f43f86462a10c491fec925f604322ee7a6a
Shenole Latimer <shenole@gmail.com>
2019-03-22 00:05:36 -0400

    Completed build of Navbar component and RegisterLogin component. Need to create forms for Registering and Login.

---
+++ Web/Beastbase/src/components/layout/Navbar.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg charcoal">
+          <a className="navbar-brand text-light" href="#">
+            Beastbase
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex justify-content-end"
+            id="navbarNavAltMarkup"
+          >
+            <ul className="navbar-nav d-flex justify-content-end">
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  About Beastbase
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Developers
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
