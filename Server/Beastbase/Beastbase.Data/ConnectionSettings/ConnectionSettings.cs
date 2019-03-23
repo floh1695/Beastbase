572a2f546878d5e98492890f0ee1b80d65656596
Charles Bayley <cbayley95@gmail.com>
2019-03-23 17:05:59 -0400

    Not sure if we will actually need port

--- Server/Beastbase/Beastbase.Data/ConnectionSettings/ConnectionSettings.cs
+++ Server/Beastbase/Beastbase.Data/ConnectionSettings/ConnectionSettings.cs
@@ -4,7 +4,7 @@
 	{
 		public string Server { get; set; }
 		public string Database { get; set; }
-		public int? Port { get; set; }
+		//public int? Port { get; set; }
 		public string UserName { get; set; }
 		public string Password { get; set; }
 	}
