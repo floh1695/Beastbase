22197fa9761613e7feac9f7f7912c0a69cbaf6c4
Charles Bayley <cbayley95@gmail.com>
2019-03-23 16:51:52 -0400

    Added model to represent database connection settings

---
+++ Server/Beastbase/Beastbase.Data/ConnectionSettings/ConnectionSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Beastbase.Data.ConnectionSettings
+{
+	public class ConnectionSettings
+	{
+		public string Server { get; set; }
+		public string Database { get; set; }
+		public int? Port { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+	}
+}
