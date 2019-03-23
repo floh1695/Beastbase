bc323fc2b2a65ba6f13fb8ec56f424fb81ee4735
Charles Bayley <cbayley95@gmail.com>
2019-03-23 16:56:51 -0400

    Added extensions for converting between connection strings and ConnectionSettings

---
+++ Server/Beastbase/Beastbase.Data/ConnectionSettings/Extensions/ConnectionSettingsToConnectionStringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+namespace Beastbase.Data.ConnectionSettings.Extensions
+{
+	public static class ConnectionSettingsToConnectionStringExtension
+	{
+		public static string ToConnectionString(this ConnectionSettings settings)
+		{
+			var builder = new SqlConnectionStringBuilder();
+
+			builder["Server"] = settings.Server;
+			builder["Database"] = settings.Database;
+
+			//if (settings.Port.HasValue)
+			//{
+			//	builder["Port"] = settings.Port;
+			//}
+
+			builder["User Id"] = settings.UserName;
+			builder["Password"] = settings.Password;
+
+			return builder.ConnectionString;
+		}
+	}
+}
---
+++ Server/Beastbase/Beastbase.Data/ConnectionSettings/Extensions/ConnectionStringToConnectionSettingsExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace Beastbase.Data.ConnectionSettings.Extensions
+{
+	public static class ConnectionStringToConnectionSettingsExtension
+	{
+		public static ConnectionSettings ToConnectionSettings(this string connectionString)
+		{
+			var builder = new SqlConnectionStringBuilder(connectionString);
+
+			var server = builder["Server"].ToString();
+			var database = builder["Database"].ToString();
+			//var port = builder["Port"];
+			var username = builder["User Id"].ToString();
+			var password = builder["Password"].ToString();
+
+			var settings = new ConnectionSettings
+			{
+				Server = server,
+				Database = database,
+				//Port = port,
+				UserName = username,
+				Password = password,
+			};
+
+			return settings;
+		}
+	}
+}
