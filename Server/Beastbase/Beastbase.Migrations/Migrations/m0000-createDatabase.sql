abbc7454a8f30b30d7cb9b1f6db5bd8f3b6c2752
Charles Bayley <cbayley95@gmail.com>
2019-03-25 22:36:24 -0400

    First migration script. Needs safety features

---
+++ Server/Beastbase/Beastbase.Migrations/Migrations/m0000-createDatabase.sql
@@ -0,0 +1,23 @@
+﻿create database Beastbase;
+go
+
+use Beastbase;
+go
+
+create table dbo.Migrations
+(
+	id uniqueidentifier primary key default newid(),
+	migrationNumber int not null,
+	migrationDescription nvarchar(255) not null,
+	runDate datetime not null default getdate()
+);
+go
+
+insert into dbo.Migrations 
+(
+	migrationNumber,
+	migrationDescription
+)
+VALUES 
+	(0, 'Create database and then create migration table');
+go
