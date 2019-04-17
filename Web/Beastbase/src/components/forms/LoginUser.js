7f5037d7fa6a3f48bfd0a42aa98f162bc98c23ba
Shenole Latimer <shenole@gmail.com>
2019-04-17 14:25:04 -0400

    Registartion and login forms now accept inputs and can console log it out on submission.

--- Web/Beastbase/src/components/forms/LoginUser.js
+++ Web/Beastbase/src/components/forms/LoginUser.js
@@ -5,20 +5,28 @@ class LoginUser extends Component {
         loginUserName: '',
         loginPassword: ''
     }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+
     render() {
         const { loginUserName, loginPassword } = this.state;
         return (
             <div>
                 <div className="card center-screen" id="login-card">
                     <div className="card-body text-light">
-                        <form className="round-preset pt-4">
+                        <form className="round-preset pt-4" onSubmit={this.onSubmit}>
                             <div className="form-group text-left">
                                 <label htmlFor="login-username">User Name</label>
-                                <input type="text" className="form-control" id="login-username" name="loginUseName" value={loginUserName} />
+                                <input type="text" className="form-control" id="login-username" name="loginUserName" value={loginUserName} onChange={this.onChange} />
                             </div>
                             <div className="form-group text-left">
                                 <label htmlFor="login-password">Password</label>
-                                <input type="password" className="form-control" id="login-password" name="loginPassword" value={loginPassword} />
+                                <input type="password" className="form-control" id="login-password" name="loginPassword" value={loginPassword} onChange={this.onChange} />
                             </div>
                             <button type="submit" className="btn btn-success mt-4">Submit</button>
                         </form>
--- Web/Beastbase/src/components/forms/RegisterUser.js
+++ Web/Beastbase/src/components/forms/RegisterUser.js
@@ -8,28 +8,35 @@ class RegisterUser extends Component {
         confirmPassword: ''
     }
 
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         const { username, email, password, confirmPassword } = this.state;
         return (
             <div className="full-height">
                 <div className="card center-screen" id="register-card">
                     <div className="card-body text-light">
-                        <form className="round-preset pt-4">
+                        <form className="round-preset pt-4" onSubmit={this.onSubmit}>
                             <div className="form-group text-left">
                                 <label htmlFor="input-username">User Name</label>
-                                <input type="email" className="form-control" id="input-username" name="username" value={username} />
+                                <input type="email" className="form-control" id="input-username" name="username" value={username} onChange={this.onChange} />
                             </div>
                             <div className="form-group text-left">
                                 <label htmlFor="input-email">Email</label>
-                                <input type="password" className="form-control" id="input-email" name="email" value={email} />
+                                <input type="password" className="form-control" id="input-email" name="email" value={email} onChange={this.onChange} />
                             </div>
                             <div className="form-group text-left">
                                 <label htmlFor="input-password">Password</label>
-                                <input type="email" className="form-control" id="input-password" name="password" value={password} />
+                                <input type="email" className="form-control" id="input-password" name="password" value={password} onChange={this.onChange} />
                             </div>
                             <div className="form-group text-left">
                                 <label htmlFor="input-password-confirm">Confirm Password</label>
-                                <input type="password" className="form-control" id="input-password-confirm" name="confirmPassword" value={confirmPassword} />
+                                <input type="password" className="form-control" id="input-password-confirm" name="confirmPassword" value={confirmPassword} onChange={this.onChange} />
                             </div>
                             <button type="submit" className="btn btn-success mt-4">Submit</button>
                         </form>
